e35ef83d3556a60d444003ee9838452a56488eb4
U15Diana <udianaa2001@gmail.com>
2023-07-25 22:32:01 +0300

    Работа с классами и функциями

---
+++ sandbox/src/main/java/ru/stq/pft/sandbox/Square.java
@@ -0,0 +1,12 @@
+package ru.stq.pft.sandbox;
+
+public class Square {
+    public double a;
+
+    public Square(double a) {
+        this.a = a;
+    }
+    public double area() {
+        return this.a * this.a;
+    }
+}
--- sandbox/src/main/java/ru/stq/pft/sandbox/prog.java
+++ sandbox/src/main/java/ru/stq/pft/sandbox/prog.java
@@ -4,9 +4,12 @@ public class prog {
     public static void main(String [] args) {
         hello("Diana");
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь: " + square(a, b));
+        Square s = new Square(5.0);
+        Rectangle r = new Rectangle(5.0, 6.0);
+
+        System.out.println("Площадь квадрата: " + s.area());
+        System.out.println("Площадь прямоугольника: " + r.area());
+
     }
 
     public static void hello(String somebody) {
