40479650eac03b7d2acf81f48e58c42c80db2277
U15Diana <udianaa2001@gmail.com>
2023-07-26 00:00:55 +0300

    Добавлен ответ на задание 2

---
+++ sandbox/src/main/java/ru/stq/pft/sandbox/Point.java
@@ -0,0 +1,23 @@
+package ru.stq.pft.sandbox;
+
+public class Point {
+    public double x;
+    public double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point p) {
+        return Math.sqrt((this.x-p.x)*(this.x-p.x) + (this.y-p.y)*(this.y-p.y));
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ",y=" + y +
+                '}';
+    }
+}
--- sandbox/src/main/java/ru/stq/pft/sandbox/prog.java
+++ sandbox/src/main/java/ru/stq/pft/sandbox/prog.java
@@ -10,6 +10,15 @@ public class prog {
         System.out.println("Площадь квадрата: " + s.area());
         System.out.println("Площадь прямоугольника: " + r.area());
 
+        Point p1 = new Point(5.0, 1.0);
+        Point p2 = new Point(4.0, 6.0);
+
+        System.out.println("3. Расстояние между точками " + p1.toString() + " и " + p2.toString() + ": " + distance(p1, p2));
+        System.out.println("4. Расстояние между точками " + p1.toString() + " и " + p2.toString() + ": " + p1.distance(p2));
+    }
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt((p1.x-p2.x)*(p1.x-p2.x) + (p1.y-p2.y)*(p1.y-p2.y));
     }
 
     public static void hello(String somebody) {
