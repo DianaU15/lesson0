2c1987ff24327d90078d2f3c832f994e4da99922
U15Diana <udianaa2001@gmail.com>
2023-07-25 22:04:05 +0300

    Добавлено вычисление площади прямоугольника

--- sandbox/src/main/java/ru/stq/pft/sandbox/prog.java
+++ sandbox/src/main/java/ru/stq/pft/sandbox/prog.java
@@ -1,7 +1,19 @@
 package ru.stq.pft.sandbox;
 
 public class prog {
-public static void main(String [] args) {
-System.out.println("Hello, world!");
-}
+    public static void main(String [] args) {
+        hello("Diana");
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь: " + square(a, b));
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double square(double a, double b) {
+        return a*b;
+    }
 }
