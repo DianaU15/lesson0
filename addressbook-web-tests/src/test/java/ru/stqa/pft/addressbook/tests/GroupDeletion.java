8a843d8aad632038d1d67d26d3dcce46baaa3d96
U15Diana <udianaa2001@gmail.com>
2023-08-10 23:42:15 +0300

    added a precondition for GroupDeletion

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletion.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletion.java
@@ -1,12 +1,16 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.module.GroupData;
 
 public class GroupDeletion extends TestBase{
 
   @Test
   public void testDeleteGroup() throws Exception {
     app.getNavigationHelper().goToGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroup();
     app.getGroupHelper().returnToGroupPage();
