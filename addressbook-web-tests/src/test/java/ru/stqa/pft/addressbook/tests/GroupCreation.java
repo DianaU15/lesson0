d5b8a50cc74cb748f737339b6ff21882ba20f193
U15Diana <udianaa2001@gmail.com>
2023-08-16 23:54:02 +0300

    added checking the number of elements

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreation.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreation.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.module.GroupData;
 
@@ -10,7 +11,10 @@ public class GroupCreation extends TestBase{
   @Test
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().goToGroupPage();
+    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().createGroup(new GroupData("test1", null, null));
-    }
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before+1);
+  }
 
 }
